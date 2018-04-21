465f11b25f1199572e26c69ebe724466fc2bc155
Pratik Kumar <pratikyoff@gmail.com>
2018-04-21 13:49:10 +0530

    A basic webpage

---
+++ Host/Controllers/WebPageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Host.Controllers
+{
+    [Route("")]
+    public class WebPageController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return File(new FileStream("wwwroot/index.html", FileMode.Open), "text/html");
+        }
+    }
+}
